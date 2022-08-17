3560efa96696b210109815c0e63fd05e1d21b5b3
EunjoungAhn <aej9300@gmail.com>
2022-08-17 19:45:58 +0900

    section2 스프링의 Environment 컴포넌트 = 인터페이스와 함께 Value 라는 애노테이션으로 스프링이 제공하는 외부 설정 정보를 가져다가 쓰기

--- todos/src/main/java/todoapp/web/TodoController.java
+++ todos/src/main/java/todoapp/web/TodoController.java
@@ -1,5 +1,7 @@
 package todoapp.web;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,17 +9,40 @@ import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import todoapp.web.model.SiteProperties;
+
 @Controller
 public class TodoController {
+	
+	private Environment environment;
+	private String siteAuthor;
+	
+	/*생성자를 통해서 의존 관계 주입 후, 외부 설정값 가지고 오기
+	public TodoController(Environment environment) {
+		this.environment = environment;
+	}
+	*/
+	
+	//애노테이션으로 설정값 가져오기
+	public TodoController(Environment environment, @Value("${site.author}") String sitAuthor) {
+		this.environment = environment;
+		this.siteAuthor = sitAuthor;
+	}
 	
 	//웹 요청을 처리하기 위한 핸들러를 작성
 	@RequestMapping("/todos")
 	public ModelAndView todos() throws Exception {
+		SiteProperties site = new SiteProperties();
+		//site.setAuthor(environment.getProperty("site.author"));
+		site.setAuthor(siteAuthor);
+		site.setDescription("스프링 MVC 할 일 관리 앱");
+		
 		ModelAndView mav = new ModelAndView();
+		mav.addObject("site", site);
 		mav.setViewName("todos");
 		
-		ViewResolver viewResolver = new InternalResourceViewResolver();
-		View view = viewResolver.resolveViewName("todos", null);
+		//ViewResolver viewResolver = new InternalResourceViewResolver();
+		//View view = viewResolver.resolveViewName("todos", null);
 		
 		// ViewResolver의 규현체들은 대부분 접두사와 접미사라는 형식의 설정을 가질 수 있다.
 		// fullViewName = prefix + "todos" + suffix
