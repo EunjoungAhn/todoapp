1ae1e63cbb65e3459721e926cf1d5b74426afb82
EunjoungAhn <aej9300@gmail.com>
2022-08-17 21:32:16 +0900

    section2_5 스프링이 제공하는 초기 데이터 설정방법

---
+++ todos/src/main/java/todoapp/web/TodoRestController.java
@@ -0,0 +1,25 @@
+package todoapp.web;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import todoapp.core.todos.application.TodoFinder;
+import todoapp.core.todos.domain.Todo;
+
+@RestController
+public class TodoRestController {
+	
+	private final TodoFinder finder;
+	
+	public TodoRestController(TodoFinder finder) {
+		this.finder = finder;
+	}
+
+	@RequestMapping("/api/todos")
+	public List<Todo> list(){
+		return Collections.emptyList();
+	}
+}
