3dae7d9a7b389b701771064372f3803e40d7a6b3
EunjoungAhn <aej9300@gmail.com>
2022-08-18 16:40:46 +0900

    section3_1 콘텐트 협상 메커니즘2

--- todos/src/main/java/todoapp/web/WebMvcConfiguration.java
+++ todos/src/main/java/todoapp/web/WebMvcConfiguration.java
@@ -1,11 +1,18 @@
 package todoapp.web;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.ObjectToStringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
+import todoapp.core.todos.domain.Todo;
 import todoapp.web.TodoController.TodoCsvViewResolver.TodoCsvView;
 
 /**
@@ -30,18 +37,37 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
 	@Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
+		//스프링 WebMvcConfigurer 인터페이스의 viewResolver 사용
 		//registry.viewResolver(new TodoController.TodoCsvViewResolver());
 		
         // registry.enableContentNegotiation();
         // 위와 같이 직접 설정하면, 스프링부트가 구성한 ContentNegotiatingViewResolver 전략이 무시된다.
     }
+	
+	//새로운 메시지 컨버터 등록
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		DefaultConversionService conversionService = new DefaultConversionService();
+		//왠만하면 DefaultConversionService로 마무리가 되지만
+		//Todo 객체를 스트링으로 변환해 줄 수 있는 특별한 컨버터가 더 필요하다.
+		conversionService.addConverter(new Converter<Todo, String>() {
 
+			@Override
+			public String convert(Todo source) {
+				return source.toString();
+			}
+		});
+		
+		converters.add(new ObjectToStringHttpMessageConverter(null));
+	}
+	
+	//새로운 뷰 적용
 	@Bean(name = "todos")
 	public TodoCsvView todoCsvView() {
 		return new TodoCsvView();
 	}
-	
-    /**
+
+	/**
      * 스프링부트가 생성한 ContentNegotiatingViewResolver를 조작할 목적으로 작성된 컴포넌트
      */
     public static class ContentNegotiationCustomizer {
