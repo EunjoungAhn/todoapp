da6412bf8aa7867a9a4071e7228b963d33ff3fdf
EunjoungAhn <aej9300@gmail.com>
2022-08-19 23:58:03 +0900

    Section6_2 로그인 화면에서 Servlet API로 클라이언트의 값 가져오기

--- todos/src/main/java/todoapp/web/LoginController.java
+++ todos/src/main/java/todoapp/web/LoginController.java
@@ -1,8 +1,13 @@
 package todoapp.web;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import todoapp.web.model.SiteProperties;
 
@@ -10,6 +15,7 @@ import todoapp.web.model.SiteProperties;
 public class LoginController {
 	
 	private final SiteProperties siteProperties;
+	private final Logger logger = LoggerFactory.getLogger(getClass()); 
 	
 	public LoginController(SiteProperties siteProperties) {
 		this.siteProperties = siteProperties;
@@ -23,7 +29,18 @@ public class LoginController {
 	*/
 
 	@GetMapping("/login")
-	public void login() {
+	public void loginForm() {
+	
+	}
+	
+	@PostMapping("/login")
+	//Servlet API로 클라이언트의 값 가져오기
+	public void loginProcess(HttpServletRequest request) {
+		//서버로 사용자의 입력 값을 가져온다.
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		logger.debug("login command: {}, {},", username, password);
 	}
 	
 }
