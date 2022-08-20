3a03ddba09f91bf3edebaf0fea8005c85b881ab8
EunjoungAhn <aej9300@gmail.com>
2022-08-20 22:32:10 +0900

    section6_3 사용자 로그인 로직 작성하기

--- todos/src/main/java/todoapp/web/LoginController.java
+++ todos/src/main/java/todoapp/web/LoginController.java
@@ -10,15 +10,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import todoapp.core.user.application.UserPasswordVerifier;
+import todoapp.core.user.application.UserRegistration;
+import todoapp.core.user.domain.UserEntityNotFoundException;
 import todoapp.web.model.SiteProperties;
 
 @Controller
 public class LoginController {
 	
+	private final UserPasswordVerifier userPasswordVerifier;
+	private final UserRegistration userRegistration;
 	private final SiteProperties siteProperties;
 	private final Logger logger = LoggerFactory.getLogger(getClass()); 
 	
-	public LoginController(SiteProperties siteProperties) {
+	public LoginController(SiteProperties siteProperties, UserPasswordVerifier userPasswordVerifier, UserRegistration userRegistration) {
+		this.userPasswordVerifier = userPasswordVerifier;
+		this.userRegistration = userRegistration;
 		this.siteProperties = siteProperties;
 	}
 	
@@ -36,8 +43,18 @@ public class LoginController {
 	
 	@PostMapping("/login")
 	//Servlet API로 클라이언트의 값 가져오기
-	public void loginProcess(LoginCommand command) {
+	public String loginProcess(LoginCommand command) {
 		logger.debug("login command: {}", command);
+		
+		try {
+			//1. 사용자 저장소에 사용자가 있을 경우: 비밀번호 확인 후 로그인 처리
+			userPasswordVerifier.verify(command.getUsername() , command.getPassword());
+		} catch(UserEntityNotFoundException error) {
+			//2. 사용자가 없는 경우: 회원가입 처리 후 로그인 처리
+			userRegistration.join(command.getUsername() , command.getPassword());
+		}
+		
+		return "redirect:/todos";
 	}
 	
 	static class LoginCommand{
