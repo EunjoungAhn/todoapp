574e1237087c376ee2123e13b743673597793311
EunjoungAhn <aej9300@gmail.com>
2022-08-22 23:50:52 +0900

    section6_17 로그인한 사용자만 프로필 조회 api 접근 허용하기

--- todos/src/main/java/todoapp/web/UserRestController.java
+++ todos/src/main/java/todoapp/web/UserRestController.java
@@ -1,18 +1,11 @@
 package todoapp.web;
 
-import java.util.Objects;
+import javax.annotation.security.RolesAllowed;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
-import todoapp.core.user.domain.User;
 import todoapp.security.UserSession;
-import todoapp.security.UserSessionRepository;
 import todoapp.web.model.UserProfile;
 
 @RestController
@@ -27,19 +20,9 @@ public class UserRestController {
 	*/
 	
 	@GetMapping("/api/user/profile")
-	//public ResponseEntity<UserProfile> userProfile(@SessionAttribute("user") User user){
-	public ResponseEntity<UserProfile> userProfile(UserSession userSession){
-		//UserSession userSession = userSessionRepository.get(); //세션 정보를 핸드러의 인수로 받기 때문에 필요 없다.
-		//사용자 정보를 세션 정보로 부터 가져오기
-		//User user = (User) session.getAttribute("user");
-		//if(Objects.nonNull(user)) {
-		if(Objects.nonNull(userSession)) {
-			//로그인 사용자의 정보가 세션에 들어 있다면 반환
-			//return ResponseEntity.ok(new UserProfile(user)); //200
-			return ResponseEntity.ok(new UserProfile(userSession.getUser())); //200
-		}
-		//user 객체가 null 이라면
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); //401
+	@RolesAllowed("ROLE_USER")
+	public UserProfile userProfile(UserSession userSession){
+		return new UserProfile(userSession.getUser());
 	}
 }
  
