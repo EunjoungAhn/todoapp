b76dfc8a4976db3457369fb85f68494038c2042b
EunjoungAhn <aej9300@gmail.com>
2022-08-22 20:31:02 +0900

    section6_15 오류 응답 status(상태코드) 500에러에서 설정 적용하기

--- todos/src/main/java/todoapp/web/GlobalControllerAdvice.java
+++ todos/src/main/java/todoapp/web/GlobalControllerAdvice.java
@@ -1,8 +1,16 @@
 package todoapp.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import todoapp.security.AccessDeniedException;
+import todoapp.security.UnauthorizedAccessException;
 import todoapp.web.model.SiteProperties;
 
 @ControllerAdvice
@@ -18,4 +26,22 @@ public class GlobalControllerAdvice {
 	public SiteProperties siteProperties() {
 		return siteProperties;
 	}
+	
+	/*
+	@ExceptionHandler(UnauthorizedAccessException.class)
+	public ResponseEntity<Map<String, Object>> handleUnauthorizedAccessException(UnauthorizedAccessException error){
+		Map<String, Object> attributes = new HashMap<>();
+		attributes.put("error", error.getClass().getSimpleName());
+		attributes.put("message", error.getMessage());
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(attributes);//권한이 없을때는 401
+	}
+	
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<Map<String, Object>> handleUnauthorizedAccessException(AccessDeniedException error){
+		Map<String, Object> attributes = new HashMap<>();
+		attributes.put("error", error.getClass().getSimpleName());
+		attributes.put("message", error.getMessage());
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(attributes);//403
+	}
+	*/
 }
