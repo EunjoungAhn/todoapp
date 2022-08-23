17772e3d62f160282730f06ac1e820a534e24edb
EunjoungAhn <aej9300@gmail.com>
2022-08-23 23:38:22 +0900

    appendix_4 E2E(end-to-end) 테스트를 돕는 WebTestClient

--- todos/src/test/java/todoapp/LoginControllerTests.java
+++ todos/src/test/java/todoapp/LoginControllerTests.java
@@ -1,64 +1,45 @@
 package todoapp;
 
+import org.hamcrest.core.StringEndsWith;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.BodyInserters.FormInserter;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import todoapp.core.user.domain.User;
-import todoapp.security.UserSession;
+import reactor.core.publisher.Mono;
 import todoapp.security.UserSessionRepository;
 import todoapp.web.LoginController;
 import todoapp.web.model.SiteProperties;
 
-//@ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes = TodosApplication.class)
-//@WebAppConfiguration
-//@SpringJUnitConfig(TodosApplication.class)// 위의 ExtendWith, ContextConfiguration 역할을 다 감당해 주는 애노테이션이다.
-@SpringJUnitWebConfig(TodosApplication.class)//ExtendWith, ContextConfiguration, WebAppConfiguration 3가지의 기능을 다 담당하는 애노테이션이다.
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT) //랜덤 포트를 쓰겠다.
 public class LoginControllerTests {
-	
-	/*
-	private AnnotationConfigServletWebServerApplicationContext applicationContext;
-	
-	//Test가 실행되기 전
-	@BeforeEach
-	void setUp() throws Exception{
-		//스프링 컨테이너 생성
-		applicationContext = new AnnotationConfigServletWebServerApplicationContext(TodosApplication.class);
-	}
-	
-	//테스트가 실행된 후
-	@AfterEach
-	void tearDown() throws Exception{
-		//스프링 컨테이너 종료
-		if(Objects.nonNull(applicationContext)) {
-			applicationContext.close();
-			
-		}
-	}
-	*/
 	
 	@Autowired ApplicationContext applicationContext;
 	@Autowired UserSessionRepository userSessionRepository;
 	@Autowired LoginController controller;
 	
-	MockMvc mockMvc;
+	WebClient webClient;
+	WebTestClient webTestClient;
 	
+	//스프링 부트가 내장된 서버로 application 서버를 실행하고 이때 랜덤하게 port를 쓴다.
+	//랜덤 port를 @LocalServerPort 로 받아와서 사용한다. 
 	@BeforeEach
-	void setUp(WebApplicationContext wac) {//스프링이 테스트 메서드의 파라미터를 받을 수 있게 제공해 준다.
-		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	void setUp(@LocalServerPort int port) {//스프링이 테스트 메서드의 파라미터를 받을 수 있게 제공해 준다.
+		webClient = WebClient.create("http://localhost:" + port);
+		//webTestClient 만드는 3가지 방법이 있으며, 1. applicationContext 2. 컨트롤러(핸들러) 3. 서버
+		//3. 서버를 대상으로 만들어 본다.
+		webTestClient = WebTestClient.bindToServer().baseUrl("http://localhost:"+port).build();
 	}
 	
 	@Test
@@ -69,56 +50,40 @@ public class LoginControllerTests {
 	
 	@Test //특별히 Mock 테스트의 경우 @Autowired를 붙여서 인자를 받아와야 한다.
 	void 인증되지않은_사용자가_로그인화면에_접근하면_로그인화면을_보여준다(@Autowired SiteProperties siteProperties) throws Exception {
-		/*스프링의 테스트 컨텍스트가 해주기 때문제 제거 가능하다.
-		MockHttpServletRequest requset = new MockHttpServletRequest();
-		MockHttpServletResponse response = new MockHttpServletResponse();
-		
-		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(requset, response));
-		
-		LoginController controller = applicationContext.getBean(LoginController.class);
-		 */
-		//Assertions.assertEquals("login", controller.loginForm()); //컨트롤러가 아닌 MockMVC로 동작 하도록 수정
-		//RequestContextHolder.resetRequestAttributes();
-		
-		RequestBuilder request = MockMvcRequestBuilders.get("/login");
-		
-		//응답 값이 200 인지? 뷰 이름이 login인지? site 라는 모델이 있는지? 그 값이 site값이 맞는지?
-		mockMvc.perform(request).andExpectAll(
-				MockMvcResultMatchers.status().isOk(),
-				MockMvcResultMatchers.view().name("login"),
-				MockMvcResultMatchers.model().attribute("site", siteProperties)
+		webTestClient.get().uri("/login").exchange().expectAll(
+			spec -> spec.expectStatus().isOk(),
+			spec -> spec.expectBody()//사용을 위해 login.html 파일을 login.xhtml 로 변경해야한다.
+				.xpath("//input[@name='username']").exists()
+				.xpath("//input[@name='password']").exists()
+				.xpath("//a[text()='"+siteProperties.getAuthor()+"']").exists()
 		);
 	}
 	
 	@Test
 	void 인증된_사용자가_로그인화면에_접근하면_할일관리화면으로_전환시킨다() throws Exception {
-		/*스프링의 테스트 컨텍스트가 해주기 때문제 제거 가능하다.
-		MockHttpServletRequest requset = new MockHttpServletRequest();
-		MockHttpServletResponse response = new MockHttpServletResponse();
+		//기본적으로 http는 무상태 이다. 쿠키로 매니징을 해줘야 하지만 안하고 있어서 
+		//쿠키 제공을 위해 작성한다.
+		MultiValueMap<String, String> cookieStore = new LinkedMultiValueMap<>();
 		
-		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(requset, response));
-		
-		//로그인 되었을때 처리
-		//UserSessionRepository userRepository = applicationContext.getBean(UserSessionRepository.class);
-		userSessionRepository.set(new UserSession(new User("tester", "")));
-		 */
-		
-		//LoginController controller = applicationContext.getBean(LoginController.class);
-		//Assertions.assertEquals("redirect:/todos", controller.loginForm()); //전체 테스트를 위해 수정
-		
-		//RequestContextHolder.resetRequestAttributes();
-		
-		//get 요청으로 로그인 요청이간후
-		RequestBuilder request = MockMvcRequestBuilders.get("/login").with(mockRequest -> {
-			//로그인이 되어 있는 상태를 처리하는 코드
-			RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(mockRequest));
-			userSessionRepository.set(new UserSession(new User("tester", "")));
-			return mockRequest;
-		});
-		
-		mockMvc.perform(request).andExpectAll(
-				MockMvcResultMatchers.status().is3xxRedirection(),
-				MockMvcResultMatchers.view().name("redirect:/todos")
+		//1. 사용자가 로그인을 한다.
+		MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+			formData.add("username", "user");
+			formData.add("password", "password");
+		//로그인 요청을 보내기 위해 바디를 준비
+		FormInserter<String> formBody = BodyInserters.fromFormData(formData);
+		webClient.post().uri("/login").body(formBody).exchangeToMono(response ->{
+			//쿠키를 반복하면서 쿠키를 복제
+			response.cookies().forEach((name, cookies) -> {
+				cookies.forEach(cookie -> cookieStore.add(name, cookie.getValue()));
+			});
+			return Mono.empty();
+		}).block();
+			
+		//2. 로그인된 사용자가 로그인 화면 접근시 상황을 검증한다.
+		webTestClient.get().uri("/login").cookies(cookies -> cookies.addAll(cookieStore)).exchange().expectAll(
+				sepc -> sepc.expectStatus().is3xxRedirection(),
+				sepc -> sepc.expectHeader().value(HttpHeaders.LOCATION, StringEndsWith.endsWith("/todos"))
+				
 		);
 	}
 	
